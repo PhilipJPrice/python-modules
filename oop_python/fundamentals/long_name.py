be61665e546945c04ec9ad216a9a73b62207cd7b
PhilipJPrice <philip.price.1994@gmail.com>
2023-02-13 08:58:22 -0800

    ADD: docstring

--- oop_python/fundamentals/long_name.py
+++ oop_python/fundamentals/long_name.py
@@ -1,4 +1,10 @@
 class LongNameDict(dict):
+    """
+    varName = LongNameDict() # Assigns Class to Variable
+    varName['key'] = value # Assigns a key and value pair to class variable
+    varname['key2'] = value2 # Assigns a key and value pair to class variable
+    varName.longest_key() # Calls Function
+    """
     def longest_key(self):
         longest = None
         for key in self:
